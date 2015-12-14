05f3774759164811cf2c6d88af1a7076173c8e32
yy <yy@vishvesh-HP-EliteBook-8440p.(none)>
2015-12-14 15:04:43 -0500

    Sequential Code Updated

--- SequentialBFS.c
+++ SequentialBFS.c
@@ -6,53 +6,50 @@
 typedef enum boolean{false, true} bool;
 char** adj;
 bool visited[ MAX ];
-main()
-{
-    int i, v=1;
+
+void create_graph();
+void bfs(int source);
+void display();
+
+int main(){
+    int i, source=1;
     create_graph();
     for ( i = 0;i < MAX;i++ )
 	visited[ i ] = false;
-   clock_t start = clock(), diff;
-bfs(v);
-diff = clock() - start;
-
-int msec = diff * 1000 / CLOCKS_PER_SEC;
-printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
- } /*End of main()*/
+    clock_t start = clock(), diff;
+    bfs(source);
+    diff = clock() - start;
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
+    return 0; 
+} /*End of main()*/
  
-create_graph()
-{
-  //printf("1\n");
-  adj = (char **) malloc(sizeof(char*)*MAX);
-
-  int ind=0; 
-  for(;ind<MAX;ind++)
-     adj[ind] = (char *) malloc(MAX*sizeof(char));
-   srand(100);
-   int dens = 60;
-      long long int noofOnes=0;
-      long long int i2,j2,k2;
-      printf("2\n");
-         for(i2=0; i2< MAX; i2++){
-	   for(j2=i2; j2< MAX; j2++){
-//              printf("%d e ",i2); 	     
-              int rand1 = rand()%100;
-              if(rand1>=dens){
+void create_graph(){
+    adj = (char **) malloc(sizeof(char*)*MAX);
+    int ind=0; 
+    for(;ind<MAX;ind++)
+        adj[ind] = (char *) malloc(MAX*sizeof(char));
+    srand(100);
+    int dens = 60;
+    long long int noofOnes=0;
+    long long int i2,j2,k2;
+    printf("2\n");
+    for(i2=0; i2< MAX; i2++){
+        for(j2=i2; j2< MAX; j2++){
+            int rand1 = rand()%100;
+            if(rand1>=dens){
                 adj[i2][j2] = 1;
 		adj[j2][i2] = 1;
                 noofOnes++;
-              }else{
+            }else{
 		adj[i2][j2] = 0;
 		adj[j2][i2] = 0;
-	      }
-	   }
-      }
-      printf("3\n");
-     // display();
-
+	    }
+	}
+    }
 } /*End of create_graph()*/
  
-display()
+void display()
 {
     int i, j;
    printf("inside display\n");
@@ -66,46 +63,28 @@ display()
 } /*End of display()*/
 
  
-bfs( int v )
+void bfs( int v )
 {
     long long int i, front, rear;
     int* que = (int*) malloc(sizeof(int)*MAX);
     front = rear = -1;
- 
     printf( "%d ", v );
     visited[ v ] = true;
     rear++;
     front++;
     que[ rear ] = v;
- 
-    while ( front <= rear )
-        {
-            v = que[ front ]; /* delete from queue */
-            front++;
- 
-            for ( i = 0;i < MAX;i++ )
-                {
-                    /* Check for adjacent unvisited nodes */
- 
-                    if ( adj[ v ][ i ] == 1 && visited[ i ] == false )
-                        {
-                            printf( "%d ", i );
-                            visited[ i ] = true;
-                            rear++;
-                            que[ rear ] = i;
-                        }
-                }
-	}
+    while ( front <= rear ){
+        v = que[ front ]; /* delete from queue */
+        front++;
+        for ( i = 0;i < MAX;i++ ){
+            /* Check for adjacent unvisited nodes */
+            if ( adj[ v ][ i ] == 1 && visited[ i ] == false ){
+                visited[ i ] = true;
+                rear++;
+                que[ rear ] = i;
+             }   
+        }
+    }
 } /*End of bfs()*/
  
-adj_nodes( int v )
-{
-    int i;
- 
-    for ( i = 0;i < MAX;i++ )
-        if ( adj[ v ][ i ] == 1 )
-            printf( "%d ", i );
- 
-    printf( "\n" );
-} /*End of adj_nodes()*/
  
